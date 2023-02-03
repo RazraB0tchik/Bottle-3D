7ca65d13c49817f0670ea364b856d7edb9897144
RazraB0tchik <100565729+RazraB0tchik@users.noreply.github.com>
2023-02-03 13:33:03 +1000

    comit2

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Bottle</h1>
+    <BottleElement/>
+</template>
+
+<script>
+import BottleElement from "@/components/BottleElement";
+
+export default {
+  name: "App",
+  components: {
+    BottleElement
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
